aa70c05b43395b4380ef3b0fa4f5c177ca039f5b
nakib555 <140257355+nakib555@users.noreply.github.com>
2026-01-10 13:02:10 +0600

    feat: Setup ESLint and React Compiler
    Integrate ESLint for code quality checks and React Compiler for performance optimizations.
    
    This includes:
    - Adding ESLint and React Compiler dependencies.
    - Configuring ESLint to enforce React Compiler rules.
    - Enabling the React Compiler Babel plugin in Vite.
    - Introducing a new `lint` script to the package.json.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import million from "million/compiler";
 
+const ReactCompilerConfig = {
+  target: '19' 
+};
+
 export default defineConfig({
   plugins: [
     million.vite({ auto: true }),
-    react()
+    react({
+      babel: {
+        plugins: [
+          ["babel-plugin-react-compiler", ReactCompilerConfig],
+        ],
+      },
+    })
   ],
   base: '/',
   resolve: {
